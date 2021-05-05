f7b472ca86228deb85a8b2e4af6af6ac2842e6f6
unknown <dabesaw999@live.com.mx>
2021-05-05 13:23:16 -0500

    Practica 3 - Pull con rama develop

---
+++ Practica3/src/main/java/com/distribuidos/cliente/backend/Libro.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.distribuidos.cliente.backend;
+
+/**
+ *
+ * @author Dave
+ */
+public class Libro {
+    
+}
